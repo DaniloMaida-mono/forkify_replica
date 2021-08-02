fa333676380321265100938c581998992c840f15
DannyM-web <danilo.maida95@libero.it>
2021-08-02 15:15:44 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const App = () => {
+    return (
+        <>
+            <header></header>
+            <main>
+            </main>
+            <footer></footer>
+        </>
+    );
+}
+
+export default App
---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const path = require("path");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const tailwindcss = require('tailwindcss')
+const autoprefixer = require('autoprefixer')
+
+module.exports = {
+    entry: path.join(__dirname, "src", "index.js"),
+    output: {
+        path: path.join(__dirname, "public"),
+        filename: "./js/[name].[contenthash].js"
+    },
+    mode: process.env.NODE_ENV || "development",
+    resolve: { modules: [path.resolve(__dirname, "src"), "node_modules"] },
+    devServer: {
+        contentBase: path.join(__dirname, "src"),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: ["babel-loader"]
+            },
+            {
+                test: /\.(css|scss|sass)$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader',
+                ],
+            },
+            {
+                test: /\.(jpg|jpeg|png|svg)$/,
+                use: ["file-loader"]
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, "src", "index.html"),
+        }),
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({
+            filename: 'css/[name].[contenthash].css',
+            chunkFilename: '[id].[contenthash].css',
+        }),
+    ],
+    optimization: {
+        splitChunks: { chunks: "all" }
+    },
+};
